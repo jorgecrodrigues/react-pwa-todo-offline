27c04d250fa8f91e9be3da4440be9cc035f62165
Jorge Rodrigues <jorgerodrigues9@outlook.com>
2023-06-30 18:11:11 -0300

    Added optimistic todo update

--- src/features/Todo/hooks/usePostTodo.ts
+++ src/features/Todo/hooks/usePostTodo.ts
@@ -1,11 +1,46 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { Todo } from "../types"
 
-export default function usePostTodo() {
-  return useMutation((todo: any) => fetch('https://jsonplaceholder.typicode.com/todos', {
+function mutationFn(todo: Omit<Todo, 'id'>) {
+  return fetch('https://jsonplaceholder.typicode.com/todos', {
     method: 'POST',
     body: JSON.stringify(todo),
     headers: {
         'Content-type': 'application/json; charset=UTF-8',
     },
-    }).then(res => res.json()))
+    }).then(res => res.json())
+}
+
+export default function usePostTodo() {
+  const queryClient = useQueryClient()
+
+  return useMutation(mutationFn, {
+    onMutate: async (newTodo) => {
+      // Random string to use as a unique id
+      const id = Math.random().toString(36)
+      // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries(['todoList'])
+      // Snapshot the previous value
+      const previousTodos = queryClient.getQueryData<Todo[]>(['todoList'])
+      // Optimistically update to the new value
+      queryClient.setQueryData<Todo[]>(['todoList'], (old: Todo[] | undefined) => {
+        if (old) {
+          return [{ ...newTodo, id }, ...old]
+        }
+        return [{ ...newTodo, id }]
+      })
+      // Return a context object with the snapshotted value
+      return { previousTodos }
+    },
+    onError(_, __, context) {
+      // If there was an error, roll back to the snapshot
+      if (context?.previousTodos) {
+        queryClient.setQueryData(['todoList'], context.previousTodos)
+      }
+    },
+    onSettled() {
+      // Always refetch after error or success:
+      queryClient.invalidateQueries({ queryKey: ['todoList']})
+    },
+  })
 }
--- src/features/Todo/pages/List.tsx
+++ src/features/Todo/pages/List.tsx
@@ -1,9 +1,11 @@
 import useGetTodo from "../hooks/useGetTodo"
 
 import Create from "./Create"
+import { Todo } from "../types"
 
 export default function List() {
     const { data, isLoading, isError } = useGetTodo()
+
     return (
         <div>
             <h1>Todo List</h1>
@@ -11,7 +13,7 @@ export default function List() {
             {isLoading && <div>Loading...</div>}
             {isError && <div style={{color: 'red'}}>Something went wrong</div>}
             <ul>
-                {data?.map((todo: any) => (
+                {data?.map((todo: Todo) => (
                     <li key={todo.id}>{todo.title}</li>
                 ))}
             </ul>
